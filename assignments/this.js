5d8fbacefb3da93d77d3295c6be8b78069ce67e2
ofega <fegaeze@gmail.com>
2019-08-21 13:50:34 +0100

    this keyword principles explained

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,15 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+    The value of this is actually not assigned to anything until an object calls a method where the `this` keyword is used. That is, the value of `this` is based on the object that invokes the method/function where `this` is defined: 
+    
+    * 1. Calling a function in the global environment without attaching it to an object as a method, or binding it explicitly, sets the global object(window) as `this`.
+
+    * 2. Calling a method on an object where `this` is referenced, sets the `this` keyword to the object to the left of the method call.
+    
+    * 3. When invoking a constructor function with the `new` operator, the `this` keyword gets set to the newly created object.
+
+    * 4. Functions can be bound to any objects of our choosing, by using either call or apply to set the `this` keyword at function calls, or bind to return a function that has been bound to a specified object. This sets `this` to the bound objects.
 *
 * write out a code example of each explanation above
 */
